163bb4f8233a071a1da7257d35f919b3d648b510
PavelKrasikov <127337558+PavelKrasikov@users.noreply.github.com>
2023-03-14 20:12:35 +0300

    Final solution

---
+++ Таблица кубов числа.cs
@@ -0,0 +1,8 @@
+﻿// ввод данных
+Console.WriteLine("Введите число: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Таблица кубов чисел от 1 до " + a + ":");
+
+ for (int i = 1; i <= a; i++)
+
+    Console.WriteLine(i + " в кубе = " + Math.Pow(i,3));
