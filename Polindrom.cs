a67170dab0b5b8631194fb4ef36a5c7d21c5d477
PavelKrasikov <127337558+PavelKrasikov@users.noreply.github.com>
2023-03-14 21:37:39 +0300

    First solution

---
+++ Polindrom.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number / 10000 % 10; // First digit
+int b = number / 1000 % 10; // Second digit
+int c = number / 10 % 10; // Fours digit
+int d = number % 10; // Five digit
+
+if (a == d && b == c)
+    Console.WriteLine("Число - полиндром");
+
+else
+    Console.WriteLine("Число - не полиндром");
+    
